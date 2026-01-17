056c715c0cb4d181a0811f3f110b5df1fbad977e
amorfide <73080161+Amorfide@users.noreply.github.com>
2026-01-17 20:14:43 +0000

    used preventDefault() to prevent refresh of page and stored input value into the variable text

---
+++ js-file.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector("button");
+let text = document.querySelector("#item");
+
+btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("I pressed it");
+    console.log(text.value);
+    });
