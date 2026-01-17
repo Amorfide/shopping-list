580d03db69035e2e47cf5c8fdcd6e0f1d1982d2a
amorfide <73080161+Amorfide@users.noreply.github.com>
2026-01-17 22:23:26 +0000

    cleaned up the code, and added functionality to delete items from the list. Fixed incorrect code, appended to listItem instead of ul.

--- js-file.js
+++ js-file.js
@@ -4,17 +4,25 @@ let ul = document.querySelector("ul");
 
 btn.addEventListener("click", (event) => {
     event.preventDefault();
-    console.log("I pressed it");
-    console.log(text.value);
-    myItem = text.value;
+
+    const myItem = text.value;
+
     text.value = "";
+
     const listItem = document.createElement("li");
     const span = document.createElement("span");
     const button = document.createElement("button");
-    ul.appendChild(span);
-    ul.appendChild(button);
+
     span.textContent = myItem;
     button.textContent = "Delete";
+
+    button.addEventListener("click", (event) => {
+        listItem.remove();
     });
+
+    listItem.appendChild(span);
+    listItem.appendChild(button);
+    ul.appendChild(listItem);
+});
 
 
